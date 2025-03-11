d7bb09813376d85c74a9c2409ee34f1e6728b36d
JoestarGagan <151487971+JoestarGagan@users.noreply.github.com>
2025-03-11 12:03:55 +0530

    Add files via upload

---
+++ newfile.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello Jenkins, This is PES2UG22CS078\n";
+  cout << "I am working with jenkins pipeline   \n";
+  return 0;
+}

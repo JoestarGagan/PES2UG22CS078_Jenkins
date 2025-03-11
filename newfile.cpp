76a6f7773e68de52de832442444023afdf759db8
JoestarGagan <151487971+JoestarGagan@users.noreply.github.com>
2025-03-11 12:06:22 +0530

    Delete newfile.cpp

--- newfile.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello Jenkins, This is PES2UG22CS078\n";
-  cout << "I am working with jenkins pipeline   \n";
-  return 0;
-}
